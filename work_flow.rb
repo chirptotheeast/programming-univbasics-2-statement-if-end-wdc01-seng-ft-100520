5bf963ee3c2c4b721d80ea1d2a90eb4718a88607
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-09-10 21:19:57 +0000

    Automatically backed up by Learn

--- work_flow.rb
+++ work_flow.rb
@@ -12,3 +12,12 @@ if run_code_inside
   puts "code inside"
 end
 puts "Code after if...end"
+
+chance_of_rain = 0.2
+puts "Let's go outside!"
+if chance_of_rain > 0.5
+  puts "Pack an umbrella!"
+else
+  puts "Enjoy the find day!"
+end
+puts "Oh, and always wear sunscreen!"
